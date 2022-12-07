2695d3003d375489f28bcd575df217bdb3953d88
atere21 <tosinateret@gmail.com>
2022-12-07 11:59:41 -0800

    task

--- Day1/Exercises/Debugging.py
+++ Day1/Exercises/Debugging.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env/python3
 
+#Fix code:
+
+print("Day 1 - String Manipulation")
+
+print("String Concatenation is done with the \"+\" sign.")
+
+print('e.g. print("Hello " + "world")')
+
+print("New lines can be created with a backslash and n.")
+
+#fixed code:
+
+print(Day 1 - String Manipulation")                        <-- missing a quote
+print("String Concatenation is done with the "+" sign.")   <-- missing escape sequence for quotes
+print('e.g. print("Hello " + "world")')                  <-- uneccessary indentation
+print(("New lines can be created with a backslash and n.") <-- uneccessary extra open parenthesis
