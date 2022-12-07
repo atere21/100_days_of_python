b1f1bb164eb39aa090992fd9713db84ced2664c7
atere21 <tosinateret@gmail.com>
2022-12-07 11:34:09 -0800

    task 1

---
+++ Day1/Exercises/printing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python3
+
+#simple exercise of printing output to the screen
+
+print("Day 1 - Python Print Function")
+
+print("The function is declared like this:")
+
+print("print('what to print')")
