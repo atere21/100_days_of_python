16f5de88a79549faeca401a0e8d940232886f4b6
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-07 14:04:46 -0600

    Update Debugging.py

--- Day1/Exercises/Debugging.py
+++ Day1/Exercises/Debugging.py
@@ -15,4 +15,4 @@ print("New lines can be created with a backslash and n.")
 print(Day 1 - String Manipulation")                        <-- missing a quote
 print("String Concatenation is done with the "+" sign.")   <-- missing escape sequence for quotes
 print('e.g. print("Hello " + "world")')                  <-- uneccessary indentation
-print(("New lines can be created with a backslash and n.") <-- uneccessary extra open parenthesis
+print("New lines can be created with a backslash and n.") <-- uneccessary extra open parenthesis
