2a147c4f1c60d39f2eb819a5b3c0b369c750e4e7
atere21 <tosinateret@gmail.com>
2022-12-07 13:10:10 -0800

    task

---
+++ Day1/Exercises/Input_function.py
@@ -0,0 +1,12 @@
+#program that prints the number of characters in a user's name
+
+#get name from user
+
+name = input("What is your name?")
+#output length of name to console
+
+print(len(name))
+
+#compiling both:
+
+#print(len(input("What is your name?")))
