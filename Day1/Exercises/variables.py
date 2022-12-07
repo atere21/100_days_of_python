760c9fdf95d8cb9313eb0fefb3a2ef63a35e2907
atere21 <tosinateret@gmail.com>
2022-12-07 13:27:59 -0800

    task

---
+++ Day1/Exercises/variables.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+
+a = input("a: ")
+
+b = input("b: ")
+
+#temp = a # using temporary variable to hold the current value of a before changing a's actual value
+
+a = b;
+
+b = temp
+
+# 🚨 Don't change the code below 👇
+
+print("a: " + a)
+
+print("b: " + b)
