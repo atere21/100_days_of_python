7dfee7e8debdcc8ef44e47881d8eede6257f59e6
atere21 <tosinateret@gmail.com>
2022-12-08 00:50:03 -0800

    task

---
+++ Day2/Exercises/Data_types.py
@@ -0,0 +1,37 @@
+# 🚨 Don't change the code below 👇
+
+two_digit_number = input("Type a two digit number: ")
+
+# 🚨 Don't change the code above 👆
+
+
+
+####################################
+
+#Write your code below this line 👇
+
+
+
+#Strings are sequences of characters that can be individually accessed using brackets ([]) and an index.
+
+
+
+#Digit, Index
+
+
+
+#ex: 15
+
+
+
+# 1: index 0
+
+# 5: index 1
+
+
+
+# int() converts a given string into a integer value.
+
+
+
+print(int(two_digit_number[0]) + int(two_digit_number[1])) # returns an integer
