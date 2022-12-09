6e7d5941c186b6c5ff3cc5afe0ff913608a28138
atere21 <tosinateret@gmail.com>
2022-12-08 18:47:04 -0800

    task

---
+++ Day3/Exercises/Odd_Even.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+
+number = int(input("Which number do you want to check? "))
+
+# 🚨 Don't change the code above 👆
+
+
+
+#Write your code below this line 👇
+
+
+
+# ternary operator in python (basically a mini if statement)
+
+print("This is an odd number." if number % 2 != 0 else "This is an even number.")
