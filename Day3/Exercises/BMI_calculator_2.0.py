755919026675d80cd51c22b8745da1f7a026d759
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-09 03:47:29 -0600

    Update BMI_calculator_2.0.py

--- Day3/Exercises/BMI_calculator_2.0.py
+++ Day3/Exercises/BMI_calculator_2.0.py
@@ -5,23 +5,18 @@ weight = float(input("enter your weight in kg: "))
 
 #Write your code below this line 👇
 
-
-#pow() is a built in function that raises a number to a power
-
 #float() converts strings into floating point numbers
-bmi = weight / pow(height, 2)
+BMI = round (weight / height ** 2)
 
-category = "";
-
-if(bmi < 18.5):
-    print(f"Your BMI is {int(round(bmi, 0))}, you are underweight.")
-elif(bmi >= 18.5 and bmi < 25):
-    print(f"Your BMI is {int(round(bmi, 0))}, you have a normal weight.")
-elif(bmi >= 25 and bmi < 30):
-    print(f"Your BMI is {int(round(bmi, 0))}, you are slightly overweight.")
-elif(bmi >= 30 and bmi < 35):
-    print(f"Your BMI is {int(round(bmi, 0))}, you are obese.")
-elif(bmi > 35):
-    print(f"Your BMI is {int(round(bmi, 0))}, you are clinically obese.")
+if(BMI < 18.5):
+  print(f"Your BMI is {BMI}, you are underweight.")
+elif(BMI >= 18.5 and BMI < 25):
+    print(f"Your BMI is {BMI}, you have a normal weight.")
+elif(BMI >= 25 and BMI < 30):
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif(BMI >= 30 and bmi < 35):
+    print(f"Your BMI is {BMI}, you are obese.")
+elif(BMI > 35):
+    print(f"Your BMI is {BMI}, you are clinically obese.")
     
 # Note: Be careful of the sample input, as it has height and weight swapped from code produced from editor. 
