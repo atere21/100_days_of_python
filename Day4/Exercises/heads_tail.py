3930c9af1bdd698cef1da20212e6af860018acfb
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-12 02:53:06 -0600

    Update heads_tail.py

--- Day4/Exercises/heads_tail.py
+++ Day4/Exercises/heads_tail.py
@@ -4,9 +4,7 @@ import random
 
 import time
 
-#Hint: Remember to import the random module here at the top of the file. 🎲
-
-     
+#Hint: Remember to import the random module here at the top of the file. 🎲 
 
      # 🚨 Don't change the code below 👇
 
@@ -15,21 +13,10 @@ import time
      random.seed(test_seed)
 
       # 🚨 Don't change the code above 👆 It's only for testing your code.
-
-         
-
          #Write the rest of your code below this line 👇
 
-
-
          # 1 - heads
-
          # 0 - tails
-
-
-
          randNumber = random.randint(0,1)
-
-
 
          print("Heads" if randNumber == 1 else "Tails")
